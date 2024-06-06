663a2a2fcd7109b17ffc60c961027df496ddf763
Marcio Messias <marciomessiasdebarros@gmail.com>
2024-06-06 06:20:28 -0300

    refactor: delete import product unused

--- src/usecase/product/create/create.product.integration.spec.ts
+++ src/usecase/product/create/create.product.integration.spec.ts
@@ -1,5 +1,4 @@
 import { Sequelize } from "sequelize-typescript";
-import Product from "../../../domain/product/entity/product";
 import CreateProductUseCase from "./create.product.usecase"
 import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
 import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
