82848465abb4f9c96116c6d1375cd9c677431600
Marcio Messias <marciomessiasdebarros@gmail.com>
2024-06-08 04:33:32 -0300

    feat: implemented presenters customer list

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
 import CreateCustomerUsecase from "../../../usecase/customer/create/create.customer.usecase";
 import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import CustomerListPresenter from "../presenters/customer.list.presenter";
 
 export const customerRoute = express.Router();
 
@@ -29,7 +30,12 @@ customerRoute.get("/", async (req: Request, res: Response) => {
   try {
     const useCase = new ListCustomerUseCase(new CustomerRepository());
     const output = await useCase.execute({});
-    res.send(output);
+
+    res.format({
+      json: async () => res.send(output),
+      xml: async () => res.send(CustomerListPresenter.toXml(output))
+    });
+
   } catch (error) {
     res.status(500).send(error);
   }
